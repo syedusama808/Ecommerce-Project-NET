3e1f92bcde362dd04f08ed261a4b892677143169
Ali Usama <ali.usama@multinet.com.pk>
2024-11-20 22:59:15 +0500

    SQL backup added.

---
+++ Ecommerce-Project-NET/SqlBackup.sql
@@ -0,0 +1,33 @@
+﻿USE [Ecommerce]
+GO
+
+/****** Object:  Table [dbo].[Category]    Script Date: 11/20/2024 10:57:30 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Category](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](100) NOT NULL,
+	[CategoryDescription] [nvarchar](max) NULL,
+	[CategoryImage] [nvarchar](max) NOT NULL,
+	[CategoryID] [int] NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedOn] [datetime] NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Category] ADD  CONSTRAINT [DF_Category_CreatedOn]  DEFAULT (getdate()) FOR [CreatedOn]
+GO
+
+ALTER TABLE [dbo].[Category] ADD  CONSTRAINT [DF_Category_Active]  DEFAULT ((1)) FOR [Active]
+GO
+
+
